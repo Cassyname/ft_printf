4c1915e17fc2791eb7149f962ac3a61b8f5e3475
Cassyname <119490174+Cassyname@users.noreply.github.com>
2023-03-17 12:22:58 +0100

    final

---
+++ ft_printf_util2.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_util2.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgeoffra <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/08 13:55:26 by cgeoffra          #+#    #+#             */
+/*   Updated: 2023/03/17 11:08:14 by cgeoffra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putchar(unsigned char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+int	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (ft_putstr("(null)"));
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+	return (i);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+int	ft_putnbr(int n, int i)
+{
+	if (n == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return (11);
+	}
+	if (n < 0)
+	{
+		ft_putchar('-');
+		n = (-1 * n);
+		i++;
+	}
+	if (n >= 10)
+	{
+		i = ft_putnbr(n / 10, i + 1);
+		ft_putchar((n % 10) + '0');
+	}
+	else
+	{
+		i++;
+		ft_putchar(n + '0');
+	}
+	return (i);
+}
